b2ef91707f3d9be7d1e4415f1179b87b143fc8c5
Kotaro Kamashima <kotaro0726@gmail.com>
2022-11-05 09:52:33 +0900

    Update: Run app and display black screen

--- Source/Rampage/ViewController.swift
+++ Source/Rampage/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let imageView = UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpImageView()
+    }
+
+    func setUpImageView() -> Void {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .black
     }
 }
