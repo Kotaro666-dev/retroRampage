a66e6a838d2515ea816c72f04b29de35ecb26119
Kotaro Kamashima <kotaro0726@gmail.com>
2022-11-05 09:58:07 +0900

    Update: Add Color

---
+++ Source/Engine/Color.swift
@@ -0,0 +1,27 @@
+//
+// Created by Kotaro Kamashima on 2022/11/05.
+// Copyright (c) 2022 Nick Lockwood. All rights reserved.
+//
+
+import Foundation
+
+public struct Color {
+    public var r, g, b, a: UInt8
+
+    public init(r: UInt8, g: UInt8, b: UInt8, a: UInt8 = 255) {
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+    }
+}
+
+public extension Color {
+    static let clear = Color(r: 0, g: 0, b: 0, a: 0)
+    static let black = Color(r: 0, g: 0, b: 0)
+    static let white = Color(r: 255, g: 255, b: 255)
+    static let gray = Color(r: 192, g: 192, b: 192)
+    static let red = Color(r: 255, g: 0, b: 0)
+    static let green = Color(r: 0, g: 255, b: 0)
+    static let blue = Color(r: 0, g: 0, b: 255)
+}
