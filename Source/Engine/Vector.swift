1ce97851a4f14b05752383f8905a68617fb35082
Kotaro Kamashima <kotaro0726@gmail.com>
2022-11-05 13:58:54 +0900

    Update: Add Vector which has extensions with overloads operator

---
+++ Source/Engine/Vector.swift
@@ -0,0 +1,63 @@
+//
+// Created by Kotaro Kamashima on 2022/11/05.
+// Copyright (c) 2022 Nick Lockwood. All rights reserved.
+//
+
+public struct Vector {
+    public var x, y: Double
+
+    public init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
+
+public extension Vector {
+    static func +(lhs: Vector, rhs: Vector) -> Vector {
+        Vector(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+
+    static func -(lhs: Vector, rhs: Vector) -> Vector {
+        Vector(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+
+    static func *(lhs: Vector, rhs: Double) -> Vector {
+        Vector(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+
+    static func /(lhs: Vector, rhs: Double) -> Vector {
+        Vector(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+
+    static func *(lhs: Double, rhs: Vector) -> Vector {
+        Vector(x: lhs * rhs.x, y: lhs * rhs.y)
+    }
+
+    static func /(lhs: Double, rhs: Vector) -> Vector {
+        Vector(x: lhs / rhs.x, y: lhs / rhs.y)
+    }
+
+    static func +=(lhs: inout Vector, rhs: Vector) {
+        lhs.x += rhs.x
+        lhs.y += rhs.y
+    }
+
+    static func -=(lhs: inout Vector, rhs: Vector) {
+        lhs.x -= rhs.x
+        lhs.y -= rhs.y
+    }
+
+    static func *=(lhs: inout Vector, rhs: Double) {
+        lhs.x *= rhs
+        lhs.y *= rhs
+    }
+
+    static func /=(lhs: inout Vector, rhs: Double) {
+        lhs.x /= rhs
+        lhs.y /= rhs
+    }
+
+    static prefix func -(rhs: Vector) -> Vector {
+        Vector(x: -rhs.x, y: -rhs.y)
+    }
+}
